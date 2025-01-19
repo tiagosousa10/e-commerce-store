4bc2f4f4cd05a1a78278dd5359f83b8a9f3e7ee4
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-20 00:41:58 +0100

    generate tokens function created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,24 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m"
+  })
+
+  const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d"
+  })
+
+  return {accessToken, refreshToken}
+}
+
+
+const storeRefreshToken = async (userId,refreshToken) => { // Store the refresh token in Redis
+  await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7*24*60*60) // 7 days
+}
+
 
 export const signup = async (req, res) => {
   const {email,password, name} = req.body;
@@ -15,8 +35,13 @@ export const signup = async (req, res) => {
       email,
       password
     })
+
+    //authenticate
+    const {accessToken, refreshToken} =  generateTokens(user._id)
+    await storeRefreshToken(user._id, refreshToken) // Store the refresh token in Redis from user id that was just created
   
     res.status(201).json({user, message: "User created successfully"})
+
   } catch(error) {
     res.status(500).json({message: error.message})
   }
