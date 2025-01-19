6bf9b76f4c6b8b6468566432902e6c072bba9a5b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-20 00:57:01 +0100

    setCookies with access and refresh token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -7,7 +7,7 @@ const generateTokens = (userId) => {
     expiresIn: "15m"
   })
 
-  const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {
+  const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, { 
     expiresIn: "7d"
   })
 
@@ -19,6 +19,21 @@ const storeRefreshToken = async (userId,refreshToken) => { // Store the refresh 
   await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7*24*60*60) // 7 days
 }
 
+const setCookies = (res,accessToken,refreshToken) => {
+  res.cookie("accessToken", accessToken, { // Set the access token as a cookie
+    httpOnly: true, // Prevent client-side JavaScript from accessing the cookie 
+    secure: process.env.NODE_ENV === "production", // Only allow the cookie to be sent over HTTPS if NODE_ENV is set to "production
+    sameSite: "strict", // Prevent the cookie from being sent to the client on cross-site requests
+    maxAge: 15*60*1000 // Set the maximum age of the cookie to 15 minutes
+  })
+
+  res.cookie("refreshToken", refreshToken, { // Set the access token as a cookie
+    httpOnly: true, // Prevent client-side JavaScript from accessing the cookie 
+    secure: process.env.NODE_ENV === "production", // Only allow the cookie to be sent over HTTPS if NODE_ENV is set to "production
+    sameSite: "strict", // Prevent the cookie from being sent to the client on cross-site requests
+    maxAge: 7*24*60*60*1000 // MAX age of the cookie to 7 days 
+  })
+}
 
 export const signup = async (req, res) => {
   const {email,password, name} = req.body;
@@ -40,6 +55,8 @@ export const signup = async (req, res) => {
     const {accessToken, refreshToken} =  generateTokens(user._id)
     await storeRefreshToken(user._id, refreshToken) // Store the refresh token in Redis from user id that was just created
   
+    setCookies(res,accessToken,refreshToken)
+
     res.status(201).json({user, message: "User created successfully"})
 
   } catch(error) {
