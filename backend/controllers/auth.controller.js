041b5ceafd87c62a59d4945690c9f1d59efc6360
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-20 01:08:44 +0100

    logout function added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 
+
 const generateTokens = (userId) => {
   const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "15m"
@@ -19,6 +20,7 @@ const storeRefreshToken = async (userId,refreshToken) => { // Store the refresh 
   await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7*24*60*60) // 7 days
 }
 
+
 const setCookies = (res,accessToken,refreshToken) => {
   res.cookie("accessToken", accessToken, { // Set the access token as a cookie
     httpOnly: true, // Prevent client-side JavaScript from accessing the cookie 
@@ -34,6 +36,7 @@ const setCookies = (res,accessToken,refreshToken) => {
     maxAge: 7*24*60*60*1000 // MAX age of the cookie to 7 days 
   })
 }
+
 
 export const signup = async (req, res) => {
   const {email,password, name} = req.body;
@@ -57,7 +60,14 @@ export const signup = async (req, res) => {
   
     setCookies(res,accessToken,refreshToken)
 
-    res.status(201).json({user, message: "User created successfully"})
+    res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }, 
+      message: "User created successfully"})
 
   } catch(error) {
     res.status(500).json({message: error.message})
@@ -69,6 +79,20 @@ export const login = (req, res) => {
   res.send("login");
 };
 
-export const logout = (req, res) => {
-  res.send("logout");
+export const logout = async (req, res) => {
+ try {
+    const refreshToken = req.cookies.refreshToken; // Get the refresh token from the cookie
+    
+    if(refreshToken) {
+      const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET) // Verify the refresh token
+      await redis.del(`refresh_token:${decoded.userId}`) // Delete the refresh token from Redis
+    }
+
+    res.clearCookie("accessToken") // Clear the access token cookie
+    res.clearCookie("refreshToken") // Clear the refresh token cookie
+    res.json({message: "Logout successful"})
+
+ } catch(error) {
+    res.status(500).json({message:"Server error in Logout" , error: error.message})
+ }
 };  
