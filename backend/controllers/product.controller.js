6e883d0d8cf7d85555d8096e056dc1f1c16260d6
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-20 16:09:12 +0100

    delete product

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -69,3 +69,32 @@ export const createProduct = async (req,res) => {
     res.status(500).json({message : "Server error", error: error.message})
   }
 }
+
+
+export const deleteProduct = async (req,res) => {
+  try {
+    const product = await Product.findById(req.params.id) // from url params
+
+    if(!product) {
+      return res.status(404).json({message: "Product not found"})
+    }
+
+    if(product.image) {
+      const publicId = product.image.split("/").pop().split(".")[0] // Get the public id from the image url
+
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`) // Delete the image from Cloudinary
+        console.log("delete image from cloudinary")
+      } catch(error) { 
+        console.log("Error in deleteProduct , product.controller", error.message)
+       }       
+    }
+
+    await Product.findByIdAndDelete(req.params.id) // Delete the product from the MongoDB database
+    
+    res.json({message : "Product deleted successfully"})
+  } catch(error) {
+    console.log("Error in deleteProduct , product.controller", error.message)
+    res.status(500).json({message : "Server error", error: error.message})
+  }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router()
