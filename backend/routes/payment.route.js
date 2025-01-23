6e7720b8e7603ef13ade2b72d7417e11f366378b
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-23 01:27:04 +0100

    create stripe coupon and newCoupon to the user

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { protectRoute } from '../middleware/auth.middleware';
-import { createCheckoutSession } from '../controllers/payment.controller';
-import Coupon from '../models/coupon.model';
-import { stripe } from '../lib/stripe';
+import dotenv from 'dotenv';
+import { protectRoute } from '../middleware/auth.middleware.js';
+//import { createCheckoutSession } from '../controllers/payment.controller';
+import Coupon from '../models/coupon.model.js';
+import { stripe } from '../lib/stripe.js';
 
+dotenv.config();
 
 const router = express.Router();
 
@@ -45,14 +47,52 @@ router.post("/create-checkout-session", protectRoute, async (req,res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url:"http://localhost:3000/purchase-success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url:"http://localhost:3000/purchase-cancel"
+      success_url:`${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url:`${process.env.CLIENT_URL}/purchase-cancel`,
+      discounts:coupon ? [
+        {
+          coupon: await createStripeCoupon(coupon.discountPercentage)
+        }
+    ] : [],
+      metadata: {
+        userId: req.user._id.toString(),
+        couponCode: couponCode || ""
+      }
     })
 
+    
+    if(totalAmount >= 20000) {
+      await createNewCoupon(req.user._id)
+    }
+    res.status(200).json({id:session.id, totalAmount: totalAmount/100})
 
   } catch(error) {
 
   }
 }) 
+
+async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once"
+  })
+
+  return coupon.id;
+}
+
+// create a new coupon using the coupon model
+async function createNewCoupon(userId) { 
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2,8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+    userId:userId
+  })
+
+  await newCoupon.save(); // save the new coupon in the database
+
+  return newCoupon;
+
+}
 
 export default router;
