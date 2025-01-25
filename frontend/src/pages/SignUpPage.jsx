70a3b1420304c7eb42ae0de55e0037db0226eaa3
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-25 15:38:23 +0100

    creating animations with motion

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import { useState } from "react"
+import {Link} from 'react-router-dom'
+import {UserPlus, Mail, Lock, User, ArrowRight, Loader} from 'lucide-react'
+import {motion} from 'framer-motion'
 
 const SignUpPage = () => {
+  const loading = true;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData)
+  }
+
   return (
-    <div>SignUpPage</div>
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div 
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{opacity: 0, y:-20}}
+        animate={{opacity: 1, y:0}}
+        transition={{duration: 0.8}}
+      >
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-emerald-400">Create your account</h2>
+      </motion.div>
+
+      <motion.div 
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{opacity: 0, y:-20}}
+        animate={{opacity: 1, y:0}}
+        transition={{duration: 0.8, delay: 0.2}}
+      >
+        <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+							<label htmlFor='name' className='block text-sm font-medium text-gray-300'>
+								Full name
+							</label>
+							<div className='mt-1 relative rounded-md shadow-sm'>
+								<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+									<User className='h-5 w-5 text-gray-400' aria-hidden='true' />
+								</div>
+								<input
+									id='name'
+									type='text'
+									required
+									value={formData.name}
+									onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+									className='block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm
+									 placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm'
+									placeholder='John Doe'
+								/>
+							</div>
+            </div>
+          </form>
+        </div>
+      </motion.div>
+
+    </div>
   )
 }
 
