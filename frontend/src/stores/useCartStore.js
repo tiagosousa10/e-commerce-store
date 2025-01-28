d1dd8b63e4e8a8e6d185fa7d77d70a231fa29381
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-28 21:18:35 +0100

    get, apply and remove coupon

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -10,6 +10,26 @@ export const useCartStore = create((set,get) => ({
   subtotal:0,
   isCouponApplied: false,
 
+  getMyCoupon: async () => {
+		try {
+			const response = await axios.get("/coupons");
+			set({ coupon: response.data });
+		} catch (error) {
+			console.error("Error fetching coupon:", error);
+		}
+	},
+
+  applyCoupon: async (code) => {
+		try {
+			const response = await axios.post("/coupons/validate", { code });
+			set({ coupon: response.data, isCouponApplied: true });
+			get().calculateTotals();
+			toast.success("Coupon applied successfully");
+		} catch (error) {
+			toast.error(error.response?.data?.message || "Failed to apply coupon");
+		}
+	},
+
   getCartItems: async () => {
 		try {
 			const res = await axios.get("/cart");
