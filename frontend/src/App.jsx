ec74e590fa838223e24e3e7b3ba6455492a26d76
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-28 21:15:52 +0100

    purchase cancel page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import CategoryPage from "./pages/CategoryPage"
 import CartPage from "./pages/CartPage"
 import { useCartStore } from "./stores/useCartStore"
 import PurchaseSuccessPage from "./pages/PurchaseSuccessPage"
+import PurchaseCancelPage from "./pages/PurchaseCancelPage"
 
 function App() {
   const {user, checkAuth, checkingAuth} = useUserStore()
@@ -48,6 +49,7 @@ function App() {
         <Route path="/category/:category" element={ <CategoryPage />} />
         <Route path="/cart" element={user ?  <CartPage /> : <Navigate to="/login" />} />
         <Route path="/purchase-success" element={user ?  <PurchaseSuccessPage /> : <Navigate to="/login" />} />
+        <Route path="/purchase-cancel" element={user ?  <PurchaseCancelPage /> : <Navigate to="/login" />} />
       </Routes>
       </div>
       <Toaster/>
