9e4b60d2d714ae8ac5f657ff5c5691a2e1e15441
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-30 00:20:41 +0100

    v3

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "10mb"})); //allows you to parse req.body
 app.use(cookieParser());
@@ -29,12 +29,12 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-  })
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 
