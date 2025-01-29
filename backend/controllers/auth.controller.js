3f29d2c7e70bc87fe5abea14aab2f8d4f8966997
tiagosousa <tiagosousa.tams@hotmail.com>
2025-01-30 00:29:24 +0100

    toast updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -94,7 +94,7 @@ export const login = async (req, res) => {
         role: user.role
       })
     } else {
-      res.status(401).json({message: "Invalid email or password"})
+      res.status(400).json({message: "Invalid email or password"})
     }
 
   } catch(error) {
